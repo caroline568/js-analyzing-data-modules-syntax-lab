443a83341d50e6ab6fede9f72885ee37791a9ff5
caroline568 <caroline@DESKTOP-3N71K80.localdomain>
2026-04-15 04:30:16 -0400

    Completed combineUsers lab

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
+function combineUsers(...arrays) {
+  const mergedUsers = [].concat(...arrays);
 
+  const today = Date.today().toString("M/d/yyyy");
 
+  return {
+    users: mergedUsers,
+    merge_date: today
+  };
+}
 
-module.exports = {
-  ...(typeof combineUsers !== 'undefined' && { combineUsers })
-};
+module.exports = { combineUsers };
